e8055f9a5dcac1c3401d307294cc2697de49de20
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-17 04:05:41 +0000

    search word vertically functionality added

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+function wordSearch(letters, word) {
+
+  for (let j = 0 ; j < letters[0].length ; j++) {
+    let verticalWord = '';
+    let horizontalWord = '';
+    for (let i = 0 ; i < letters.length ; i++) {
+      verticalWord += letters[i][j];
+      horizontalWord += letters[j][i];
     }
+    if ((verticalWord.includes(word)) ||
+      (verticalWord.split('').reverse().join('').includes(word)))
+    {
+      return true;
+    }
+    if ((horizontalWord.includes(word)) ||
+      (horizontalWord.split('').reverse().join('').includes(word)))
+    {
+      return true;
+    }
+  }
+   return false;
 }
 
 module.exports = wordSearch
